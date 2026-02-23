8393233444a331162165dceda01153e590bf417c
Samuel Ybarra <sybarra2076@gmail.com>
2026-02-23 16:24:25 -0600

    Changing upload thumbnail logic to store data in db instead of base64 text

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,9 +1,28 @@
 import { existsSync, mkdirSync } from "fs";
 
 import type { ApiConfig } from "../config";
+import path from "path";
 
 export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
+}
+
+export function mediaToExtension(mediaType: string): string{
+  const fileExtension = mediaType.split("/");
+
+  if(fileExtension.length != 2){
+    return ".bin";
+  }
+
+  return `.${fileExtension[1]}`;
+}
+
+export function getAssetPath(cfg: ApiConfig, fileName: string){
+  return path.join(cfg.assetsRoot, fileName);
+}
+
+export function getAssetURL(cfg: ApiConfig, fileName: string) {
+  return `http://localhost:${cfg.port}/assets/${fileName}`;
 }
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,11 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
+import { getAssetPath, getAssetURL, mediaToExtension } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -21,31 +17,33 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  const fileData = await req.formData();
-  const fileThumbnail = fileData.get("thumbnail");
-  if(!(fileThumbnail instanceof File)){
-    throw new BadRequestError("Invalid thumbnail");
-  }
-
-  const MAX_UPLOAD_SIZE = 10 << 20;
-  if(fileThumbnail.size > MAX_UPLOAD_SIZE){
-    throw new BadRequestError("File size too large");
-  }
-
-  const thumbnail: Thumbnail = {
-    data: await fileThumbnail.arrayBuffer(),
-    mediaType: fileThumbnail.type
-  }
   const videoData = await getVideo(cfg.db, videoId);
   if(!videoData) throw new NotFoundError("video not found");
   if(videoData?.userID !== userID){
     throw new UserForbiddenError("User not authorized");
   }
 
-  const base64Encoded = Buffer.from(thumbnail.data).toString("base64");
-  const base64DataURL = `data:${thumbnail.mediaType};base64,${base64Encoded}`;
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if(!(file instanceof File)){
+    throw new BadRequestError("Invalid thumbnail");
+  }
 
-  videoData.thumbnailURL = base64DataURL;
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if(file.size > MAX_UPLOAD_SIZE){
+    throw new BadRequestError("File size too large");
+  }
+
+  const data = await file.arrayBuffer();
+  const mediaType = file.type;
+
+  const ext = mediaToExtension(mediaType);
+  const filename = `${videoId}${ext}`;
+
+  const assetPath = getAssetPath(cfg, filename);
+  await Bun.write(assetPath, data);
+
+  videoData.thumbnailURL = getAssetURL(cfg, filename);
 
   await updateVideo(cfg.db, videoData);
 
